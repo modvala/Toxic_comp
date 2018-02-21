ee691345392187295e6774ed1fe34d683c03a128
modvala <cerdgio86@gmail.com>
2018-02-21 10:13:08 +0300

    creat usefull py file

---
+++ usefull.py
@@ -0,0 +1,19 @@
+ 
+import re
+import pandas as pd
+from nltk.corpus import stopwords
+
+STOP_WORDS = stopwords.words("english")
+
+def preprocess(x):
+    x = str(x).lower()
+    x = x.replace(",000,000", "m").replace(",000", "k").replace("′", "'").replace("’", "'")\
+                           .replace("won't", "will not").replace("cannot", "can not").replace("can't", "can not")\
+                           .replace("n't", " not").replace("what's", "what is").replace("it's", "it is")\
+                           .replace("'ve", " have").replace("i'm", "i am").replace("'re", " are")\
+                           .replace("he's", "he is").replace("she's", "she is").replace("'s", " own")\
+                           .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ")\
+                           .replace("€", " euro ").replace("'ll", " will")
+    x = re.sub(r"([0-9]+)000000", r"\1m", x)
+    x = re.sub(r"([0-9]+)000", r"\1k", x)
+    return x
