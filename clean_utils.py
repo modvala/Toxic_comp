3e19e3bf84b44e09da3c7c0a8cb4de66ff4f8648
Ubuntu <cerdgio@OCTOBER.0qgviej42rpube23mwonoab0me.hx.internal.cloudapp.net>
2018-02-24 22:01:32 +0000

    24.02

--- clean_utils.py
+++ clean_utils.py
@@ -70,99 +70,6 @@ def feat_engine(data):
     return data
 
 
-    df['fewer_dates'] = df.comment_text
-
-
-
-nDAY = r'(?:[0-3]?\d)'  # day can be from 1 to 31 with a leading zero 
-nMNTH = r'(?:11|12|10|0?[1-9])' # month can be 1 to 12 with a leading zero
-nYR = r'(?:(?:19|20)\d\d)'  # I've restricted the year to being in 20th or 21st century on the basis 
-                            # that people doon't generally use all number format for old dates, but write them out 
-nDELIM = r'(?:[\/\-\._])?'  # 
-NUM_DATE = f"""
-    (?P<num_date>
-        (?:^|\D) # new bit here
-        (?:
-        # YYYY-MM-DD
-        (?:{nYR}(?P<delim1>[\/\-\._]?){nMNTH}(?P=delim1){nDAY})
-        |
-        # YYYY-DD-MM
-        (?:{nYR}(?P<delim2>[\/\-\._]?){nDAY}(?P=delim2){nMNTH})
-        |
-        # DD-MM-YYYY
-        (?:{nDAY}(?P<delim3>[\/\-\._]?){nMNTH}(?P=delim3){nYR})
-        |
-        # MM-DD-YYYY
-        (?:{nMNTH}(?P<delim4>[\/\-\._]?){nDAY}(?P=delim4){nYR})
-        )
-        (?:\D|$) # new bit here
-    )"""
-DAY = r"""
-(?:
-    # search 1st 2nd 3rd etc, or first second third
-    (?:[23]?1st|2{1,2}nd|\d{1,2}th|2?3rd|first|second|third|fourth|fifth|sixth|seventh|eighth|nineth)
-    |
-    # or just a number, but without a leading zero
-    (?:[123]?\d)
-)"""
-MONTH = r'(?:january|february|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|sept|oct|nov|dec)'
-YEAR = r"""(?:(?:[12]?\d|')?\d\d)"""
-DELIM = r'(?:\s*(?:[\s\.\-\\/,]|(?:of))\s*)'
-
-YEAR_4D = r"""(?:[12]\d\d\d)"""
-DATE_PATTERN = f"""(?P<wordy_date>
-    # non word character or start of string
-    (?:^|\W)
-        (?:
-            # match various combinations of year month and day 
-            (?:
-                # 4 digit year
-                (?:{YEAR_4D}{DELIM})?
-                    (?:
-                    # Day - Month
-                    (?:{DAY}{DELIM}{MONTH})
-                    |
-                    # Month - Day
-                    (?:{MONTH}{DELIM}{DAY})
-                    )
-                # 2 or 4 digit year
-                (?:{DELIM}{YEAR})?
-            )
-            |
-            # Month - Year (2 or 3 digit)
-            (?:{MONTH}{DELIM}{YEAR})
-        )
-    # non-word character or end of string
-    (?:$|\W)
-)"""
-
-TIME = r"""(?:
-(?:
-# first number should be 0 - 59 with optional leading zero.
-[012345]?\d
-# second number is the same following a colon
-:[012345]\d
-)
-# next we add our optional seconds number in the same format
-(?::[012345]\d)?
-# and finally add optional am or pm possibly with . and spaces
-(?:\s*(?:a|p)\.?m\.?)?
-)"""
-
-COMBINED = f"""(?P<combined>
-    (?:
-        # time followed by date, or date followed by time
-        {TIME}?{DATE_PATTERN}{TIME}?
-        |
-        # or as above but with the numeric version of the date
-        {TIME}?{NUM_DATE}{TIME}?
-    ) 
-    # or a time on its own
-    |
-    (?:{TIME})
-)"""
-
-myDate = re.compile(COMBINED, re.IGNORECASE | re.VERBOSE | re.UNICODE)
 
 def clean(comment):
     """
@@ -232,7 +139,7 @@ def clean(comment):
     
     comment=comment.lower()
     comment=re.sub("\\n","",comment)
-    comment = myDate.sub(' xxDATExx ', comment)
+    #comment = myDate.sub(' xxDATExx ', comment)
     comment=re.sub("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}","",comment)
     comment=re.sub("\[\[.*\]","",comment)
     words=tokenizer.tokenize(comment)
